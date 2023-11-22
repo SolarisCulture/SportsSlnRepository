e3b19ac3b339ad67a8a712e8a167d0548b1f698b
SolarisCulture <polinakaranxha@gmail.com>
2023-11-22 15:36:57 +0100

    successfully completed chapter 2
    - added Cart to SportShop

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -12,11 +12,17 @@ builder.Services.AddDbContext<StoreDbContext>(opt =>
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
 var app = builder.Build();
+
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+app.UseSession();
 
 app.MapControllerRoute("catpage", "{category}/Page{productPage:int}",
 	new { Controller = "Home", action = "Index" });
@@ -34,6 +40,8 @@ app.MapControllerRoute("question", "/Questions");
 
 
 app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
